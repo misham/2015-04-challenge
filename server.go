767c909ce1b4d91a34291291bba4a5fafe262c02
Brigham Toskin <brigham@ionoclast.com>
2015-04-17 16:38:21 -0700

    WIP: write count to response (throws).

--- server.go
+++ server.go
@@ -6,30 +6,54 @@ package main
 import (
 	"log"
 	"net/http"
+	"io"
 	"strconv"
 )
 
 const (
+	BASE10 = 10
 	PORT = ":8080"
 	REQUEST = "/counter/"
 )
 
 var (
-	counts map[string]int32
+	counts map[string]int
 )
 
 func init() {
-	counts = make(map[string]int32)
+	counts = make(map[string]int)
 }
 
-func serveCounter(w http.ResponseWriter, count int32) {
-	// w.Write(([]byte)strconv.FormatInt(count, 10))
+func fmtint(val int) (string) {
+	return strconv.FormatInt(int64(val), BASE10)
+}
+
+func serveCounter(w http.ResponseWriter, count int) {
+	scount := fmtint(count)
+	log.Printf("  Count: %s", scount)
+	header := w.Header()
+	header.Set("Content-Length", fmtint(1000000))
+	log.Printf("Set response content length: %s", header.Get("Content-Length"))
+	
+	_, err := io.WriteString(w, scount)
+	if err != nil {
+		log.Fatal("Writing Response: ", err)
+	}
 }
 
 func HandleRequest(w http.ResponseWriter, req *http.Request) {
-	log.Printf("Request: %s %s\n", req.Method, req.URL.Path)
-	count := counts[req.URL.Path] + 1
+	var count int
+	switch req.Method {
+	case "GET":
+		count = counts[req.URL.Path] + 1
+	case "DELETE":
+		count = 0
+	default:
+		log.Printf("Unknown request method: %s", req.Method)
+		return
+	}
 	counts[req.URL.Path] = count
+	log.Printf("Request: %s %s ", req.Method, req.URL.Path)
 	go serveCounter(w, count)
 }
 
@@ -37,10 +61,8 @@ func main() {
 	log.Print("Brig and Misham's Amazing Counter Server, ver. 1.0!!!")
 
 	http.HandleFunc(REQUEST, HandleRequest)
-	for {
-		err := http.ListenAndServe(PORT, nil)
-		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
-		}
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
