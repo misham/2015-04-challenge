2cb35d236673e28593074867bef3971b7b127a5d
Brigham Toskin <brigham@ionoclast.com>
2015-04-16 21:35:22 -0700

    Did more stuff.

--- server.go
+++ server.go
@@ -4,17 +4,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"time"
+	"strconv"
 )
 
 const (
 	PORT = ":8080"
+	REQUEST = "/counter/"
 )
 
-func handleRequest(w http.ResponseWriter, req *http.Request) {
+var (
+	counts map[string]int32
+)
+
+func init() {
+	counts = make(map[string]int32)
+}
+
+func serveCounter(w http.ResponseWriter, count int32) {
+	// w.Write(([]byte)strconv.FormatInt(count, 10))
+}
+
+func HandleRequest(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Request: %s %s\n", req.Method, req.URL.Path)
+	count := counts[req.URL.Path] + 1
+	counts[req.URL.Path] = count
+	go serveCounter(w, count)
 }
 
 func main() {
-	
+	log.Print("Brig and Misham's Amazing Counter Server, ver. 1.0!!!")
+
+	http.HandleFunc(REQUEST, HandleRequest)
+	for {
+		err := http.ListenAndServe(PORT, nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	}
 }
