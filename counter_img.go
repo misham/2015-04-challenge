180548e54775b5e3428eaaca757d6eef1c9527a4
Misha Manulis <misha@manulis.com>
2015-04-16 19:27:44 -0700

    Add image interface stub

---
+++ counter_img.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"image/png"
+	"log"
+)
+
+func GetCountPng(count uint64) image.Image {
+	var result image.Image
+
+	log.Printf("Generated image for %d\n", count)
+
+	return result
+}
