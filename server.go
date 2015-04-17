c4d5d7018d89b2ccebc3708a88c0901ec678606a
Brigham Toskin <brigham@ionoclast.com>
2015-04-16 20:02:06 -0700

    Initial commit, server.go

---
+++ server.go
@@ -0,0 +1,20 @@
+// server.go
+// brigham toskin 2015
+
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func handleRequest(w http.ResponseWriter, req *http.Request) {
+}
+
+func main() {
+	
+}
